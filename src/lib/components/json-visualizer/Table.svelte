41b04a86aa70a390cf84b270b6ed8a3c27e4730e
Radamés <r.alfaro8@icloud.com>
2023-06-17 11:50:02 -0400

    Add import alias for `components`

---
+++ src/lib/components/json-visualizer/Table.svelte
@@ -0,0 +1,1 @@
+<h1>UPDATED VIS</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import Table from '$components/json-visualizer/Table.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Table />
