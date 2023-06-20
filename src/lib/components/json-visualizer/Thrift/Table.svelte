26534f562e7aec18397dc35b0509e7cff48033a2
Radamés <r.alfaro8@icloud.com>
2023-06-20 19:20:38 -0400

    Add ability to display nested records

--- src/lib/components/json-visualizer/Thrift/Table.svelte
+++ src/lib/components/json-visualizer/Thrift/Table.svelte
@@ -2,35 +2,40 @@
 	import { THRIFT } from "./Types.js";
 
 	export let obj;
+	export let type = "rec";
 </script>
 
-<thead class="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-	<tr>
-		{#each Object.entries(obj) as column}
-			<th scope="col" class="px-6 py-3">
-				{column[0]}
-				<em>({Object.keys(column[1])[0]})</em>
-			</th>
-		{/each}
-	</tr>
-</thead>
-<tbody>
-	<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
-		{#each Object.entries(obj) as field}
-			{@const entry = Object.entries(field[1])[0]}
-			{@const key = entry[0]}
-			{@const value = entry[1]}
-			<td class="px-6 py-4">
-				{#if key == "map"}
-					MAP OBJECT (WIP)
-				{:else if key == "lst"}
-					LIST OBJECT (WIP)
-				{:else if key == "rec"}
-					RECORD OBJECT (WIP)
-				{:else}
-					{value}
-				{/if}
-			</td>
-		{/each}
-	</tr>
-</tbody>
+{#if type === "rec"}
+	<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+		<thead class="text-xs text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+			<tr>
+				{#each Object.entries(obj) as column}
+					<th scope="col" class="px-6 py-3">
+						{column[0]}
+						<em>({Object.keys(column[1])[0]})</em>
+					</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+				{#each Object.entries(obj) as field}
+					{@const entry = Object.entries(field[1])[0]}
+					{@const key = entry[0]}
+					{@const value = entry[1]}
+					<td class="px-6 py-4">
+						{#if key == "map"}
+							MAP OBJECT (WIP)
+						{:else if key == "lst"}
+							LIST OBJECT (WIP)
+						{:else if key == "rec"}
+							<svelte:self obj={value} />
+						{:else}
+							{value}
+						{/if}
+					</td>
+				{/each}
+			</tr>
+		</tbody>
+	</table>
+{/if}
--- src/lib/components/json-visualizer/Thrift/ThriftObjectDisplay.svelte
+++ src/lib/components/json-visualizer/Thrift/ThriftObjectDisplay.svelte
@@ -14,33 +14,31 @@
 	</p>
 	
 	<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-		<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-			<caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-				<dl>
-					<dt class="mt-0">Thrift Version:</dt>
-					<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jsonObject[THRIFT.FIELDS.VERSION]}</dd>
+		<caption style="width: 100em" class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+			<dl>
+				<dt class="mt-0">Thrift Version:</dt>
+				<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jsonObject[THRIFT.FIELDS.VERSION]}</dd>
 
-					<dt class="mt-6">Endpoint:</dt>
-					<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jsonObject[THRIFT.FIELDS.ENDPOINT]}</dd>
+				<dt class="mt-6">Endpoint:</dt>
+				<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jsonObject[THRIFT.FIELDS.ENDPOINT]}</dd>
 
-					<dt class="mt-6">Message Type:</dt>
-					<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jsonObject[THRIFT.FIELDS.TRANSACTION]} -
-						({THRIFT.TRANSACTION_TYPES[jsonObject[THRIFT.FIELDS.TRANSACTION]]})</dd>
+				<dt class="mt-6">Message Type:</dt>
+				<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jsonObject[THRIFT.FIELDS.TRANSACTION]} -
+					({THRIFT.TRANSACTION_TYPES[jsonObject[THRIFT.FIELDS.TRANSACTION]]})</dd>
 
-					<dt class="mt-6">Payload Type:</dt>
-					<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jsonObject[THRIFT.FIELDS.PAYLOAD_TYPE]} -
-						({THRIFT.PAYLOAD_TYPES(jsonObject[THRIFT.FIELDS.PAYLOAD_TYPE])})</dd>
+				<dt class="mt-6">Payload Type:</dt>
+				<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jsonObject[THRIFT.FIELDS.PAYLOAD_TYPE]} -
+					({THRIFT.PAYLOAD_TYPES(jsonObject[THRIFT.FIELDS.PAYLOAD_TYPE])})</dd>
 
-					<dt class="mt-6">Payload:</dt>
-					<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
-						<code>
-							{JSON.stringify(jsonObject)}
-						</code>
-					</dd>
-				</dl>
-			</caption>
-			<Table obj={jsonObject[THRIFT.FIELDS.PAYLOAD]} />
-		</table>
+				<dt class="mt-6">Payload:</dt>
+				<dd class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
+					<code>
+						{JSON.stringify(jsonObject)}
+					</code>
+				</dd>
+			</dl>
+		</caption>
+		<Table obj={jsonObject[THRIFT.FIELDS.PAYLOAD]} />
 	</div>
 {:else}
 	<p class="bg-red-200">
